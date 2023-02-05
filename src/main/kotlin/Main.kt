08528f56b666a5fb495949c2b0fcd9a906882122
Natasha K <k_natasha1818@mail.ru>
2023-02-05 15:26:47 +0700

    Сorrected

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,88 +1,91 @@
 import kotlin.text.Typography.copyright
+data class Post(
+    val id: Int,            //Идентификатор записи.
+    val ownerId: Int,        //Идентификатор владельца стены, на которой размещена запись
+    val fromId: Int,        //Идентификатор автора записи (от чьего имени опубликована запись)
+    val date: String,        //Время публикации записи в формате
+    val text: String,      //Текст записи.
+    val friendsOnly: Boolean = false,    //если запись была создана с опцией «Только для друзей»
+    val canPin: Boolean,    //Информация о том, может ли текущий пользователь закрепить запись
+    val canDelete: Boolean, //Информация о том, может ли текущий пользователь удалить запись
+    val canEdit: Boolean, //Информация о том, может ли текущий пользователь редактировать запись
+    val isFavorite: Boolean = true //true, если объект добавлен в закладки у текущего пользователя.
+) {}
 
-data class Post (
-   val id: Int,            //Идентификатор записи.
-   val ownerId:Int,        //Идентификатор владельца стены, на которой размещена запись
-   val fromId:Int,        //Идентификатор автора записи (от чьего имени опубликована запись)
-   val date: String,        //Время публикации записи в формате
-   val text: String,      //Текст записи.
-   val friendsOnly: Boolean = false,    //если запись была создана с опцией «Только для друзей»
-   val canPin:Boolean,    //Информация о том, может ли текущий пользователь закрепить запись
-   val canDelete:Boolean, //Информация о том, может ли текущий пользователь удалить запись
-   val canEdit:Boolean, //Информация о том, может ли текущий пользователь редактировать запись
-   val isFavorite:Boolean = true //true, если объект добавлен в закладки у текущего пользователя.
-
-        ) {
-
-
-}
 object Copyright {
-   fun addCopyright(linkSource: String, nameSource: String, typeSource: String): String {
-       return """$linkSource,
+    fun addCopyright(linkSource: String, nameSource: String, typeSource: String): String {
+        return """$linkSource,
                  $nameSource,
                  $typeSource""".trimMargin()
-
-   }
-
+    }
 }
-object WallService{
-     var posts = emptyArray<Post>()
+
+object WallService {
+    var posts = emptyArray<Post>()
     fun add(post: Post): Post {
         posts += post
         return posts.last()
     }
 
-    fun printPost (){
-        for ((index, post) in posts.withIndex()){
-            println("""
-       № ${post.id}
+    fun printPost() {
+        for ((index, post) in posts.withIndex()) {
+            println(
+                """
+      № ${post.id}
       номер стены ${post.ownerId}, автор записи ${post.fromId}
       дата записи: ${post.date}
      "${post.text}"
       только для друзей:${post.friendsOnly}
       закрепить:${post.canPin}, удалить: ${post.canDelete}, редактировать: ${post.canEdit}
       добавить в закладку: ${post.isFavorite}
-      источник: ${Copyright.addCopyright("https://naturae.ru/zhivotnyi-mir/mlekopitayushhie/koshki.html", "author","active")}
-                """.trimIndent())
+      источник: ${
+                    Copyright.addCopyright(
+                        "https://naturae.ru/zhivotnyi-mir/mlekopitayushhie/koshki.html",
+                        "author",
+                        "active"
+                    )
+                }
+                """.trimIndent()
+            )
         }
-
     }
 }
+
 fun main() {
-   val post = Post(
-      112,
-      6565,
-      6565,
-      "01/02/2023",
-      "Домашняя кошка - социальное животное, обладающее развитым интеллектом и способностями к общению, умеющее испытывать и выражать сложные чувства и эмоции.",
-      friendsOnly = true,
-      false,
-      false,
-      false,
-   )
+    val post = Post(
+        112,
+        6565,
+        6565,
+        "01/02/2023",
+        "Домашняя кошка - социальное животное, обладающее развитым интеллектом и способностями к общению, умеющее испытывать и выражать сложные чувства и эмоции.",
+        friendsOnly = true,
+        false,
+        false,
+        false,
+    )
 
     val post2 = Post(
-      113,
-      6565,
-      6565,
-      "01/02/2023",
-      "Размеры тела котов и кошек, в среднем, составляют около 30-60 см в длину и 20-30 см в высоту",
-      false,
-      false,
-      false,
-      false,
-   )
+        113,
+        6565,
+        6565,
+        "01/02/2023",
+        "Размеры тела котов и кошек, в среднем, составляют около 30-60 см в длину и 20-30 см в высоту",
+        false,
+        false,
+        false,
+        false,
+    )
     val post3 = Post(
-      114,
-      6565,
-      6565,
-      "01/02/2023",
-      "Кошки являются плотоядными животными, представляя собой мелких хищников. ",
-      false,
-      false,
-      false,
-      false,
-   )
+        114,
+        6565,
+        6565,
+        "01/02/2023",
+        "Кошки являются плотоядными животными, представляя собой мелких хищников. ",
+        false,
+        false,
+        false,
+        false,
+    )
     WallService.add(post)
     WallService.add(post2)
     WallService.add(post3)
